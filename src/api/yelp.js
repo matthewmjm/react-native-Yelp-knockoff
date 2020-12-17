f1a4282786c441eb8d2acdfa85926b9be3b3cd3c
matthewmjm <matthewmjm@yahoo.com>
2020-12-16 21:41:38 -0700

    Functional Search and Error Handling

---
+++ src/api/yelp.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const YELP_API_KEY = 
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`
+    }
+});
+
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async () => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    term,
+                    location: 'saint louis, mo',
+                    limit: 50
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Ooops! Something went wrong! 🥴');
+        }
+    };
+
     return (
         <View>
-            <SearchBar />
-            <Text>Search Screen</Text>
+            <SearchBar 
+                term={term}
+                onTermChange={newTerm => setTerm(newTerm)}
+                onTermSubmit={() => searchApi()}
+            />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We Have Found {results.length} Results</Text>
         </View>
     );
 };

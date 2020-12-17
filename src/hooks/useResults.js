6cd804d02f5604ee06504b8315bd7042653d3aec
matthewmjm <matthewmjm@yahoo.com>
2020-12-16 22:21:04 -0700

    run an initial search at startup

---
+++ src/hooks/useResults.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+        console.log('API call made!')
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    location: 'saint louis, mo',
+                    limit: 50,
+                    term: searchTerm
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Ooops! Something went wrong! 🥴');
+        }
+    };
+
+    // Call searchApi when component
+    // is first rendered.  BAD CODE!
+    // searchApi('pasta') <----- does an infinite loop
+    // instead, use the useEffect hook with an empty array as second argument
+    
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
+
+    return [searchApi, results, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const searchApi = async () => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    term,
-                    location: 'saint louis, mo',
-                    limit: 50
-                }
-            });
-            setResults(response.data.businesses);
-        } catch (err) {
-            setErrorMessage('Ooops! Something went wrong! 🥴');
-        }
-    };
+    const [searchApi, results, errorMessage] = useResults();
 
     return (
         <View>
             <SearchBar 
                 term={term}
                 onTermChange={newTerm => setTerm(newTerm)}
-                onTermSubmit={() => searchApi()}
+                onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We Have Found {results.length} Results</Text>
@@ -36,6 +20,6 @@ const SearchScreen = () => {
     );
 };
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
